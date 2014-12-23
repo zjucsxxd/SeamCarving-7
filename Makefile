276142e04d15758c7d6c515b9b45d89227dd968c
Hugues de Valon <hugues.devalon@free.fr>
2014-12-23 18:33:18 +0100

    Simplification du Makefile et nouvelle fonction

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 allocation_test.exe : allocation.o allocation_test.o
 	gcc objets/allocation.o objets/allocation_test.o -o allocation_test.exe
 
-allocation.o : sources/allocation.c
-	gcc sources/allocation.c -c -o objets/allocation.o
+%.o : sources/%.c
+	gcc $< -c -o objets/$@
 
-allocation_test.o : sources/allocation_test.c
-	gcc sources/allocation_test.c -c -o objets/allocation_test.o
+clean :
+	rm objets/*.o
+	
+cleanexe :
+	rm *.exe
---
+++ sources/calcul_cout.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../headers/projet.h"
+
+void calcul_cout(unsigned char** energie, unsigned int** pere,unsigned int* cout_final, int nl, int nc)
+{
+	if (pere == NULL) pere=alloue_image_int(nl, nc);
+	unsigned int** cout = alloue_image_int(nl,nc);
+	cout_final = calloc(nc, sizeof(unsigned int));
+	if (cout_final == NULL) printf("Erreur d'allocation.\n");
+	int i = 0, j = 0, k = 0;
+	for (i=0;i++;i<nl) for (j=0;j++;j<nl) cout[i][j] = 0xFFFFFFFF;
+	for (j=0;j++;j<nl) cout[0][j] = energie[0][j];
+	for (i=1;i++;i<nl)
+	{
+		for (j=0;j++;j<nc-1)
+		{
+			for (k=-1;k++;k<2)
+			{
+				if (j+k >= 0 && j+k < nc && cout[i][j] > (cout[i-1][j+k] + energie[i][j]))
+				{
+					cout[i][j] = (cout[i-1][j+k] + energie[i][j]);
+					pere[i][j] = j+k;
+				}
+			}
+		}
+	}
+	for (j=0;j++;j<nc) cout_final[j] = cout[nl-1][j];
+}
