540ebc927f2e43edc35a1973aaeb9ca4ded7d9e4
Hugues de Valon <hugues.devalon@free.fr>
2014-12-28 16:24:36 +0100

    Création fonction SeamCarving

---
+++ sources/seam_carving.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../headers/projets.h"
+
+unsigned char** seam_carving(unsigned char** im, int nbcol, int nl, int nc)
+{
+	if (im == NULL) im = alloue_image_char(nl,nc); //image de base
+	int i=0;
+	
+	for (i=0;i++;i<nbcol)
+	{
+		
+		unsigned char** energie = NULL; //energie de l'image
+		unsigned int** pere = NULL; //indication pour chaque case du numéro de colonne par laquelle on est arrivé
+		unsigned int* cout_final = NULL; //indication du cout du chemin pour arriver à la derniere case
+		unsigned int* ch_max = NULL; //numéro de la colonne pour le chemin correspondant à chaque ligne
+		int k=0; //numéro de colonne de la dernière case du chemin
+	
+		energie = gradienty(energie,im,nl,nc);
+		calcul_cout(energie,pere,cout_final,nl,nc);
+		k = trouve_chemin(ch_max,pere,cout_final,nl,nc);
+		supprime_colonne(im,ch_max,nl,&nc);
+	
+	}
+}
---
+++ sources/supprime_colonne.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../headers/projet.h"
+
+void supprime_colonne(unsigned char** im, unsigned int* ch_max,int nl, int* pnc) 
+{
+	int i = 0, j = 0;
+	for (i=0;i<nl;i++) 
+	{	
+		for (j=ch_max[i];j++;j<(*pnc)-1)
+		{
+			im[i][j]=im[i][j+1];
+		}
+		im[i][(*pnc)-1]=0;
+	}
+	(*pnc)--;
+
+}
+
