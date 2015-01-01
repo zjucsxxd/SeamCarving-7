b118d3a91ced39c4f4c41bb0e5940377144d72cb
Hugues de Valon <hugues.devalon@free.fr>
2015-01-01 15:27:15 +0100

    Finalisation sans la SDL

--- sources/prog.c
+++ sources/prog.c
@@ -4,7 +4,32 @@
 
 int main()
 {
+	//Entrée manuelle du tableau im	
+	int nc,nl,nbcol;
+	unsigned char** im = NULL;
+	unsigned char** ims = NULL;
+	unsigned char** imz = NULL;
+
+	printf("Entrez manuellement le tableau représentant l'image de base.\n");
+	printf("Nombre de ligne : ");
+	scanf("%d", &nl);
+	printf("Nombre de colonne :");
+	scanf("%d", &nc);
+
+	im = alloue_image_char(nl,nc);
+	printf("Entrez les éléments :\n");
+	for (i=0;i++;i<nl)
+	{
+		for (j=0;j++;j<nc)
+		{
+			printf("(%d,%d) : ",i,j);
+			scanf("%c", &(im[i][j]));
+		}
+	}
+
 	
-	
-	
+	printf("Réduire l'image de combien de colonnes ? ");
+	scanf("%d", &nbcol);
+	ims = seam_carving(im,nbcol,nl,nc);
+	imz = zoomx(im,nbcol,nl,nc);
 }
--- sources/seam_carving.c
+++ sources/seam_carving.c
@@ -4,10 +4,14 @@
 
 unsigned char** seam_carving(unsigned char** im, int nbcol, int nl, int nc)
 {
-	if (im == NULL) im = alloue_image_char(nl,nc); //image de base
+	unsigned char** ims = alloue_image_char(nl,nc);
+
 	int i=0;
+	int j=0;
 	
-	for (i=0;i++;i<nbcol)
+	for (i=0;i++;i<nl) for (j=0;j++;j<nc) ims[i][j]=im[i][j];
+	
+	for (j=0;j++;j<nbcol)
 	{
 		
 		unsigned char** energie = NULL; //energie de l'image
@@ -16,10 +20,11 @@ unsigned char** seam_carving(unsigned char** im, int nbcol, int nl, int nc)
 		unsigned int* ch_max = NULL; //numéro de la colonne pour le chemin correspondant à chaque ligne
 		int k=0; //numéro de colonne de la dernière case du chemin
 	
-		energie = gradienty(energie,im,nl,nc);
+		energie = gradienty(energie,ims,nl,nc);
 		calcul_cout(energie,pere,cout_final,nl,nc);
 		k = trouve_chemin(ch_max,pere,cout_final,nl,nc);
-		supprime_colonne(im,ch_max,nl,&nc);
-	
+		supprime_colonne(ims,ch_max,nl,&nc);
+
 	}
+	return ims;
 }
